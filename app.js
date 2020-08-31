850732c595c56bcea67c9d03cec05b9474e51184
NemesLaszlo <wow.laszlo@gmail.com>
2020-08-31 16:49:03 +0200

    Google auth setup and User model with functions (login,logout,callback)

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const dotenv = require('dotenv');
 const path = require('path');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./database/database');
 
 // Load config and Database connection
 dotenv.config({ path: './config/config.env' });
 connectDB();
+
+// Passport config
+require('./config/passport')(passport);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,11 +26,25 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
+// Session middleware
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 app.listen(port, () =>
   console.log(
